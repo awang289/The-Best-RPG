c64847b122e514eec481e9b1d61b009037647549
awang289 <awang289@gmail.com>
2013-11-16 21:41:40 -0500

    made methods and stuff to comply with brown's requirements

--- Character.java
+++ Character.java
@@ -8,7 +8,6 @@ public abstract class Character{
     protected int str;
     protected int def;
     protected double crt; // added crt factor
-    protected double heal; //added heal factor
     public boolean isAlive(){
 	return (HP > 0);
     }
@@ -25,7 +24,9 @@ public abstract class Character{
     }
     public int attack(Character m) { 
         //incorporated randomness 
-	int damage = (int) (((Math.random() * .15) + .85) * str - m.getDefense());
+        //I changed the attack algorithm from subtraction to division so we don't get so many 0 damages
+        //cardy just make sure no monster's defense is above 100
+	int damage = (int) (((Math.random() * .15) + .85) * 10 * str * / m.getDefense());
 	if (damage > 0) {
 	    m.lowerHP(damage);
 	    return damage;
@@ -34,22 +35,16 @@ public abstract class Character{
     }
     public int crtAttack(Character m) { 
         // to be called in YoRPG with "if (Math.random() < crt)"
-        int damage = (int) ((Math.random() * .85) * str - m.getDefense());
-        damage *= 2;
+        int damage = (int) (((Math.random() * .15) + .85) * 20 * str / m.getDefense());
         if (damage > 0) {
 	    m.lowerHP(damage);
 	    return damage;
 	}
 	else { return 0; }
     }
-    //healing method, also with a random factor
-    public int heal(Character m) {
-        amt = (int) (Math.random() * .85);
-        amt *= -1;
-        m.lowerHP(amt);
-        return amt;
-    }
+    //healing method is abstract because requirements are for one additional abstract
+    public abstract int heal();
     public abstract void specialize();
     public abstract void normalize();
-    public abstract void description(); //So I didn't have this for the hw... oops
+    public abstract void about(); //So I didn't have this for the hw... oops
 }
